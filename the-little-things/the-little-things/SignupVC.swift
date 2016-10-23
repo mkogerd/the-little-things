44a2561e67193d4d78819e9d07125930d7c5727d
Koger Darden <mkogerd@gmail.com>
2016-10-22 22:22:24 -0500

    Add loginVC and signupVC

---
+++ the-little-things/the-little-things/SignupVC.swift
@@ -0,0 +1,159 @@
+//
+//  SignupVC.swift
+//  the-little-things
+//
+//  Created by Koger Darden on 10/22/16.
+//  Copyright © 2016 HackTX2016. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signupTapped(_ sender: AnyObject) {
+       // This stuff is currently broken
+      /*  var username:NSString = txtUsername.text as NSString
+        var password:NSString = txtPassword.text as NSString
+        var confirm_password:NSString = txtConfirmPassword.text as NSString
+        
+        // Check if username and password entered
+        if ( username.isEqualToString("") || password.isEqualToString("") ) {
+            
+            var alertView:UIAlertView = UIAlertView()
+            alertView.title = "Sign Up Failed!"
+            alertView.message = "Please enter Username and Password"
+            alertView.delegate = self
+            alertView.addButtonWithTitle("OK")
+            alertView.show()
+            
+        // Check if both password entries match
+        } else if ( !password.isEqual(confirm_password) ) {
+            
+            var alertView:UIAlertView = UIAlertView()
+            alertView.title = "Sign Up Failed!"
+            alertView.message = "Passwords doesn't Match"
+            alertView.delegate = self
+            alertView.addButtonWithTitle("OK")
+            alertView.show()
+            
+        // Checks out, create and send post request
+        } else {
+            
+            var post:NSString = "username=\(username)&password=\(password)&c_password=\(confirm_password)"
+            
+            NSLog("PostData: %@",post);
+            
+            var url:NSURL = NSURL(string: "https://dipinkrishna.com/jsonsignup.php")!
+            
+            var postData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
+            
+            var postLength:NSString = String( postData.length )
+            
+            var request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+            request.HTTPMethod = "POST"
+            request.HTTPBody = postData
+            request.setValue(postLength, forHTTPHeaderField: "Content-Length")
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            
+            
+            var reponseError: NSError?
+            var response: NSURLResponse?
+            
+            var urlData: NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&reponseError)
+            
+            if ( urlData != nil ) {
+                let res = response as NSHTTPURLResponse!;
+                
+                NSLog("Response code: %ld", res.statusCode);
+                
+                if (res.statusCode >= 200 && res.statusCode < 300)
+                {
+                    var responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+                    
+                    NSLog("Response ==> %@", responseData);
+                    
+                    var error: NSError?
+                    
+                    let jsonData:NSDictionary = NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers , error: &error) as NSDictionary
+                    
+                    
+                    let success:NSInteger = jsonData.valueForKey("success") as NSInteger
+                    
+                    //[jsonData[@"success"] integerValue];
+                    
+                    NSLog("Success: %ld", success);
+                    
+                    if(success == 1)
+                    {
+                        NSLog("Sign Up SUCCESS");
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    } else {
+                        var error_msg:NSString
+                        
+                        if jsonData["error_message"] as? NSString != nil {
+                            error_msg = jsonData["error_message"] as NSString
+                        } else {
+                            error_msg = "Unknown Error"
+                        }
+                        var alertView:UIAlertView = UIAlertView()
+                        alertView.title = "Sign Up Failed!"
+                        alertView.message = error_msg
+                        alertView.delegate = self
+                        alertView.addButtonWithTitle("OK")
+                        alertView.show()
+                        
+                    }
+                    
+                } else {
+                    var alertView:UIAlertView = UIAlertView()
+                    alertView.title = "Sign Up Failed!"
+                    alertView.message = "Connection Failed"
+                    alertView.delegate = self
+                    alertView.addButtonWithTitle("OK")
+                    alertView.show()
+                }
+            }  else {
+                var alertView:UIAlertView = UIAlertView()
+                alertView.title = "Sign in Failed!"
+                alertView.message = "Connection Failure"
+                if let error = reponseError {
+                    alertView.message = (error.localizedDescription)
+                }
+                alertView.delegate = self
+                alertView.addButtonWithTitle("OK")
+                alertView.show()
+            }
+        }*/
+    }
+    
+    @IBAction func gotoLogin(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion:nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
